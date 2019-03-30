a13a6a3c8778bc5c10aee6b116d4946ffb26f44a
Zoltan <zdebre@gmail.com>
2019-03-30 22:53:00 +1300

    Remove default hello world and create controllers folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './controllers/app.controller';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
